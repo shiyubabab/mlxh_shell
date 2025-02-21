919276cd95d3a572073a2a319ae3f67a3076de0e
chenjianyu <cjyyxj1314@126.com>
2025-02-21 15:43:37 +0800

    modify input instruction of marks

--- input_processing/input_processing.c
+++ input_processing/input_processing.c
@@ -10,21 +10,27 @@
 char **remove_space(const char *str){
 	int length = strlen(str);
 	int str_num = 0, i=0;
+    int flag = 0;
 	char **args = (char **)malloc(sizeof(char *)*MAXSTRNUM);
 	while(i<length){
-		if(str[i] == ' '){
+        if(str[i] == '"') flag = (flag+1)%2;
+		if(str[i] == ' ' && !flag){
 			i++;
 			continue;
 		}
 		args[str_num] = (char *)malloc(sizeof(char)*MAXSTR);
 		int x=0;
-		while((i<length) && (str[i]!=' ')){
+		while((i<length) && (flag || str[i]!=' ')){
 			args[str_num][x++] = str[i++];
+            if(str[i] == '"'){
+                flag = (flag+1)%2;
+            }
 			if(x>MAXSTR){
 				perror("it is out of size for input word char.");
 				break;
 			}
 		}
+        printf("%s\n", args[str_num]);
 		str_num++;
 		if(str_num>MAXSTRNUM){
 			perror("it is out of size for input word.");
