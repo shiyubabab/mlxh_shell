dab2180774c4b7bf4575d1bcb384ef686ebe2e16
shiyubabab <745371486@qq.com>
2025-02-25 10:53:51 +0800

    add signal handle for ctrl+c.

--- all_of_include.h
+++ all_of_include.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "./input_processing/input_processing.h"
--- main.c
+++ main.c
@@ -7,8 +7,43 @@
 
 #include"all_of_include.h"
 
+void signal_handler(int signum){
+	printf("Received signal %d \n", signum);
+	if(signum == SIGINT){
+		//if((execlp("/usr/bin/clear","/usr/bin/clear",NULL))==-1){
+			//perror("execlp process clear error.");
+			//exit(1);
+		//}
+		printf("Please don't leave.\n");
+		printf("Are you really going to leave?\n");
+		printf("Alright then, come back next time.\n");
+		printf("hahaha, Looks like you're stuck.\n");
+		printf("You can open a new terminal. Use " GREEN BOLD "ps -ef " RESET "to find the process ID, then kill process using " GREEN BOLD "kill -9 [process ID].\n" RESET);
+		//exit(0);
+	}
+}
+
+
 int main(void){
 	while(1){
+		// signals setting
+		struct sigaction sa;
+		sigset_t mask;
+		sa.sa_handler = signal_handler;
+		sigemptyset(&mask);
+		sigaddset(&mask, SIGQUIT);
+		sa.sa_mask = mask;
+		sa.sa_flags = 0;
+		if(sigaction(SIGINT,&sa,NULL) == -1){
+			perror("sigaction");
+			return 1;
+		}
+		if(sigaction(SIGTERM,&sa,NULL) == -1){
+			perror("sigaction");
+			return 1;
+		}
+
+		//path setting
 		char currentPath[MAXPATH];
 		char str[MAXINPUT];
 		char **args;
